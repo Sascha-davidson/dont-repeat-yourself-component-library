c17161dc3e20d5afc20b3d932b6a35078b6edca3
Sascha-davidson <saschadavidson1@gmail.com>
2023-11-30 22:52:02 +0100

    add form component

---
+++ src/lib/organisms/form.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Input from "$lib/atoms/input.svelte";
+  import InputRequired from "$lib/atoms/input-required.svelte";
+  import Taxtarea from "$lib/atoms/textarea.svelte";
+
+  export let FormID, Method, Action
+</script>
+
+<form id="{FormID}" method="{Method}" action="{Action}">
+  <InputRequired
+    InputType="text"
+    InputID="AddPlayer"
+    LableFor="AddPlayer"
+    LableText="* Add player ..."
+  />
+  <section>
+    <Input
+      InputType="Date"
+      InputID="BirthDate"
+      LableFor="BirthDate"
+      LableText="Date of birth..."
+    />
+    <InputRequired
+      InputType="text"
+      InputID="JerseyNumber"
+      LableFor="JerseyNumber"
+      LableText="* Jersey number..."
+    />
+  </section>
+  <Input
+    InputType="text"
+    InputID="BirthCountry"
+    LableFor="BirthCountry"
+    LableText="* Country of birth..."
+  />
+  <Input
+    InputType="text"
+    InputID="ResidencePlace"
+    LableFor="ResidencePlace"
+    LableText="Place of residence..."
+  />
+
+  <Taxtarea InputID="Bio" LableFor="Bio" LableText="Bio..." />
+</form>
+
+<style>
+  form {
+    grid-area: form;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+    padding: 0.5rem 2rem 0.5rem 5px;
+    overflow: auto;
+    padding-top: 1.5rem;
+  }
+
+  form::-webkit-scrollbar-track {
+    border-radius: 10px;
+  }
+
+  form::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  form::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    /* -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3); */
+    background-color: #a7a7a7;
+  }
+
+  section {
+    display: flex;
+    gap: 2rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Form from "$lib/organisms/form.svelte";
+    import Button from "$lib/atoms/button.svelte";
+    import LinkButton from "$lib/atoms/link-button.svelte";
+  </script>
+  
+  <div>
+    <main>
+      <h2 id="title">Create Player</h2>
+      <p class="CreatePlayer">Create a player</p>
+      <p class="AddPlayer">Add player to team</p>
+      <Form FormID="FormID" Method="get" Action="/AllPlayers"/>
+      <Button ButtonType="submit" ButtonText="Save" FormID="FormID"/>
+      <!-- <LinkButton Routing="/" LinkText="Next Page" /> -->
+      <p class="other">other data</p>
+    </main>
+  
+    <!-- <Nav /> -->
+  </div>
+  
+  <style>
+    div {
+      display: flex;
+      height: 100%;
+    }
+  
+    main {
+      height: 100%;
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto auto 1fr auto;
+      /* column-gap: 2rem;
+      row-gap: 0; */
+      gap: 2rem;
+      padding: 4rem 4%;
+      grid-template-areas:
+        "Title Title"
+        "CreatePlayer AddPlayer"
+        "form other"
+        "button button";
+    }
+  
+    h2 {
+      grid-area: Title;
+      padding: 0 5px;
+    }
+  
+    p{
+      padding: 0 5px;
+    }
+  
+    .CreatePlayer {
+      grid-area: CreatePlayer;
+    }
+  
+    .AddPlayer {
+      grid-area: AddPlayer;
+    }
+  
+    .other {
+      grid-area: other;
+    }
+  </style>
+  
