8c49d6da9c8fb3d53a01603c396d7a2737b3ea17
Luuk <luukbrauckmann@gmail.com>
2023-12-04 12:28:31 +0100

    Create playerslist - copy playerslist component

---
+++ src/lib/organisms/PlayersList.svelte
@@ -0,0 +1,59 @@
+<script>
+	import PlayerRow from "../molecules/PlayerRow.svelte";
+	import PlayerInfo from "../molecules/PlayerInfo.svelte";
+
+	export let players = [];
+	export let reversed = false;
+
+	let player = null;
+</script>
+
+{#if player}
+	<PlayerInfo {player} {reversed} />
+{:else}
+	<table>
+		<thead>
+			<tr>
+				<th>Number</th>
+				<th>Name</th>
+				<th>Goals</th>
+				<th>Assists</th>
+				<th>Blocks</th>
+				<th>Turnovers</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each players as player}
+				<PlayerRow {player} {reversed} />
+			{/each}
+		</tbody>
+	</table>
+{/if}
+
+<style>
+	table {
+		--border-radius: 1em;
+		border-radius: var(--border-radius);
+		border-collapse: collapse;
+		background: white;
+
+		width: 100%;
+	}
+
+	thead {
+		clip: rect(0 0 0 0);
+		position: absolute;
+		clip-path: inset(50%);
+		width: 1px;
+		height: 1px;
+		overflow: hidden;
+		white-space: nowrap;
+	}
+
+	tbody {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: space-between;
+		height: 100%;
+	}
+</style>
